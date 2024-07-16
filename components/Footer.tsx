c3149ac82d3a3561f74f4130df2c1937181864bf
BereketZz <bereketzewde989@gmail.com>
2024-07-16 15:50:25 +0300

    re do

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="bereketzewde989@gmail.com">
+        <a href="mailto:bereketzewde989@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
